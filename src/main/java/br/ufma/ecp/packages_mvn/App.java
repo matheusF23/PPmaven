38ca92a63f3a8f6eaa7a42f1bdf76860d5ce91cc
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2019-10-21 15:26:30 -0300

    Criacao do projeto maven.

---
+++ src/main/java/br/ufma/ecp/packages_mvn/App.java
@@ -0,0 +1,13 @@
+package br.ufma.ecp.packages_mvn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
