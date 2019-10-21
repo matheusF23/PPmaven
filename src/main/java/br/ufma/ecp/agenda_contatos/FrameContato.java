37b709a558c411481d1cdc6683cbaedaf7a95a07
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2019-10-21 18:23:24 -0300

    Criacao da classe FrameContato

---
+++ src/main/java/br/ufma/ecp/agenda_contatos/FrameContato.java
@@ -0,0 +1,21 @@
+package br.ufma.ecp.agenda_contatos;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class FrameContato extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private JLabel lblNome;
+	private JLabel lblEmail;
+	private JLabel lblTelefone;
+	private JTextField txtNome;
+	private JTextField txtEmail;
+	private JTextField txtTelefone;
+	
+}
