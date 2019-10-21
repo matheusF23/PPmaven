e895d28b50108aa5357f1dd8f14197c2959f0369
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2019-10-21 19:15:55 -0300

    Adicao de acoes aos botoes

--- src/main/java/br/ufma/ecp/agenda_contatos/CadastraContato.java
+++ src/main/java/br/ufma/ecp/agenda_contatos/CadastraContato.java
@@ -1,25 +1,8 @@
 package br.ufma.ecp.agenda_contatos;
 
-//import java.util.List;
-
 public class CadastraContato {
 
 	public static void main(String[] args) {
-		/*
-		//Contato contato = new Contato("Gilson", "gilson@gmail.com", "996627834");	// Adiciona um contato na agenda
-		
-		ContatoDAO dao = new ContatoDAO();	// Cria uma conexao com o banco de dados permitindo operacoes no banco
-		//dao.salvaNoBanco(contato);	// salva os dados no banco
-		//dao.buscaDados();	// busca os dados no banco
-		
-		List<Contato> dados = dao.recuperarContato();
-		for(Contato con : dados) {
-			System.out.println(con);
-		}
-		
-		dao.desconectaBanco();	// Fecha conexao com o banco
-		*/
-		
 		// Iniciando a interface
 		FrameContato frame = new FrameContato();
 		frame.setVisible(true);
--- src/main/java/br/ufma/ecp/agenda_contatos/FrameContato.java
+++ src/main/java/br/ufma/ecp/agenda_contatos/FrameContato.java
@@ -1,6 +1,8 @@
 package br.ufma.ecp.agenda_contatos;
 
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -28,6 +30,7 @@ public class FrameContato extends JFrame {
 	private JPanel jpnNome;
 	private JPanel jpnEmail;
 	private JPanel jpnTelefone;
+	private JPanel jpnButton;
 
 	public FrameContato() {
 		// Config Jframe
@@ -50,22 +53,52 @@ public class FrameContato extends JFrame {
 		jpnNome = new JPanel();
 		jpnEmail = new JPanel();
 		jpnTelefone = new JPanel();
+		jpnButton = new JPanel();
 
 		// Adicionando acoes aos botoes
+		btnCadastrar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ev) {
+				Contato contato = new Contato(txtNome.getText(), txtEmail.getText(), txtTelefone.getText());
+				ContatoDAO dao = new ContatoDAO(); // Cria uma conexao com o banco de dados permitindo operacoes no
+													// banco
+				dao.salvaNoBanco(contato); // salva os dados no banco
+				dao.desconectaBanco(); // Fecha conexao com o banco
+			}
+		});
+
+		btnLimpar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				txtNome.setText("");
+				txtEmail.setText("");
+				txtTelefone.setText("");
+			}
+		});
+
+		btnSair.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
 
 		// Adicionando componentes ao JFrame
 		jpnNome.add(lblNome);
 		jpnNome.add(txtNome);
-		
+
 		jpnEmail.add(lblEmail);
 		jpnEmail.add(txtEmail);
-		
+
 		jpnTelefone.add(lblTelefone);
 		jpnTelefone.add(txtTelefone);
 		
+		jpnButton.add(btnCadastrar);
+		jpnButton.add(btnLimpar);
+		jpnButton.add(btnSair);
+
 		add(jpnNome);
 		add(jpnEmail);
 		add(jpnTelefone);
+		add(jpnButton);
+		
 
 	}
 
