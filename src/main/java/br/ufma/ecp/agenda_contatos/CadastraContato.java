5081c602efccc7d9ab263fb85dc7a190ee904dd2
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2019-10-21 17:26:28 -0300

    Adicao de comentarios

--- src/main/java/br/ufma/ecp/agenda_contatos/CadastraContato.java
+++ src/main/java/br/ufma/ecp/agenda_contatos/CadastraContato.java
@@ -7,10 +7,10 @@ public class CadastraContato {
 	public static void main(String[] args) {
 		Contato contato = new Contato("Gilson", "gilson@gmail.com", "996627834");	// Adiciona um contato na agenda
 		
-		ContatoDAO dao = new ContatoDAO();
-		dao.salvaNoBanco(contato);
-		dao.buscaDados();
-		dao.desconectaBanco();
+		ContatoDAO dao = new ContatoDAO();	// Cria uma conexao com o banco de dados permitindo operacoes no banco
+		dao.salvaNoBanco(contato);	// salva os dados no banco
+		dao.buscaDados();	// busca os dados no banco
+		dao.desconectaBanco();	// Fecha conexao com o banco
 	}
 
 }
--- src/main/java/br/ufma/ecp/agenda_contatos/ContatoDAO.java
+++ src/main/java/br/ufma/ecp/agenda_contatos/ContatoDAO.java
@@ -26,7 +26,6 @@ public class ContatoDAO {
 		try {
 			conn.close();
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} // fecha conexao com o banco
 	}
