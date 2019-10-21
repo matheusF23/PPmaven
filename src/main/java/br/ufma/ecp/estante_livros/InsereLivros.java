3cd24884dbefe2ff935114d2865709ab35ca0dd4
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2019-10-21 20:47:41 -0300

    Inicio do projeto Estante de Livros

---
+++ src/main/java/br/ufma/ecp/estante_livros/InsereLivros.java
@@ -0,0 +1,10 @@
+package br.ufma.ecp.estante_livros;
+
+public class InsereLivros {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/br/ufma/ecp/estante_livros/Livro.java
@@ -0,0 +1,5 @@
+package br.ufma.ecp.estante_livros;
+
+public class Livro {
+
+}
