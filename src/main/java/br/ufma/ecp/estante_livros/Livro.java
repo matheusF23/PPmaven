20c9dd5307ae5dd3c12e3c1644eabdae95e56016
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2019-10-21 20:55:25 -0300

    Construcao da classe Livro

--- src/main/java/br/ufma/ecp/estante_livros/Livro.java
+++ src/main/java/br/ufma/ecp/estante_livros/Livro.java
@@ -1,5 +1,38 @@
 package br.ufma.ecp.estante_livros;
 
 public class Livro {
+	private int codigo;
+	private String nome;
+	private String autor;
+	
+	public Livro(int codigo, String nome, String autor) {
+		super();
+		this.codigo = codigo;
+		this.nome = nome;
+		this.autor = autor;
+	}
 
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}	
 }
