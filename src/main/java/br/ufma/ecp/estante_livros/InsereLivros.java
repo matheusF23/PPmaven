27b0816f5223832abf1880cb1e2e39ab7f5ecca8
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2019-10-21 21:23:46 -0300

    Criacao dos metodos para salvar e buscar dados no banco

--- src/main/java/br/ufma/ecp/estante_livros/InsereLivros.java
+++ src/main/java/br/ufma/ecp/estante_livros/InsereLivros.java
@@ -3,8 +3,12 @@ package br.ufma.ecp.estante_livros;
 public class InsereLivros {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Livro livro1 = new Livro(2, "O dançarino", "Gilson");
+		
+		LivroDAO ldao = new LivroDAO();
+		
+		ldao.salvaNoBanco(livro1);
+		ldao.buscaDados();
 	}
 
 }
--- src/main/java/br/ufma/ecp/estante_livros/LivroDAO.java
+++ src/main/java/br/ufma/ecp/estante_livros/LivroDAO.java
@@ -1,5 +1,63 @@
 package br.ufma.ecp.estante_livros;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class LivroDAO {
+	private Connection conn;
+	private PreparedStatement stmt;
+	private String url = "jdbc:mysql://localhost/estante?user=root&password=1234&useTimezone=true&serverTimezone=UTC";
+
+	public LivroDAO() {
+		try {
+			// Conectando com o banco
+			conn = DriverManager.getConnection(url);
+			System.out.println("Conectado com sucesso!");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void desconectaBanco() {
+		try {
+			conn.close(); // fecha conexao com o banco
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void salvaNoBanco(Livro livro) {
+		try {
+			String sql = "insert into livros (codigo, nome, autor) values (?, ?, ?)";
+
+			stmt = conn.prepareStatement(sql);
+			stmt.setLong(1, livro.getCodigo());
+			stmt.setString(2, livro.getNome());
+			stmt.setString(3, livro.getAutor());
+			stmt.execute();
+			System.out.println("Adicionado com sucesso!");
+			stmt.close();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void buscaDados() {
+		try {
+			String sql = "select * from livros";
+			stmt = conn.prepareStatement(sql);
+			ResultSet rs = stmt.executeQuery();
+			while (rs.next()) {
+				System.out.println("cogido: " + rs.getString("codigo"));
+				System.out.println("Nome: " + rs.getString("nome"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
