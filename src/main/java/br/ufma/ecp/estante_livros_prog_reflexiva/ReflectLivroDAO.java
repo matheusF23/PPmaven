93707af46b9603fecdf2d261044287419ea62a59
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2019-11-01 19:14:50 -0300

    Atualização da classe ReflectLivroDAO

--- src/main/java/br/ufma/ecp/estante_livros_prog_reflexiva/ReflectLivroDAO.java
+++ src/main/java/br/ufma/ecp/estante_livros_prog_reflexiva/ReflectLivroDAO.java
@@ -1,58 +1,20 @@
 package br.ufma.ecp.estante_livros_prog_reflexiva;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.util.Arrays;
 import java.util.List;
 
 import br.ufma.ecp.estante_livros.Livro;
 
 public class ReflectLivroDAO implements AbstractDAO<Livro> {
-	private Connection conn;
-	private PreparedStatement stmt;
-	private final String url = "jdbc:mysql://localhost/estante?user=root&password=1234&useTimezone=true&serverTimezone=UTC";
 
-	public ReflectLivroDAO() {
-		try {
-			// Conectando com o banco
-			conn = DriverManager.getConnection(url);
-			System.out.println("Conectado ao banco!");
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public void desconectaBanco() {
-		try {
-			conn.close(); // fecha conexao com o banco
-			System.out.println("Desconexão com o banco bem sucedida!");
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
+	List<Livro> livros = Arrays.asList(new Livro(1, "O planeta dos macacos", "Pierre Boulle"),
+			new Livro(2, "172 horas na lua", "Não lembro"));
 
 	public void create(Livro livro) {
-		try {
-			String sql = "insert into livros (codigo, nome, autor) values (?, ?, ?)";
-
-			stmt = conn.prepareStatement(sql);
-			stmt.setLong(1, livro.getCodigo());
-			stmt.setString(2, livro.getNome());
-			stmt.setString(3, livro.getAutor());
-			stmt.execute();
-			System.out.println("Adicionado com sucesso!");
-			stmt.close();
-
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
 	}
 
 	public List<Livro> read() {
-		return null;
+		return livros;
 	}
-	
 
 }
