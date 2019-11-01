e09cdfb14c09c78b34aa71cf4735e94b4444c1b6
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2019-11-01 19:15:28 -0300

    Atualização da classe ReflectLivroDAO

--- src/main/java/br/ufma/ecp/estante_livros_prog_reflexiva/LivroDAO.java
+++ src/main/java/br/ufma/ecp/estante_livros_prog_reflexiva/LivroDAO.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 import br.ufma.ecp.estante_livros.Livro;
 
-public class ReflectLivroDAO implements AbstractDAO<Livro> {
+public class LivroDAO implements AbstractDAO<Livro> {
 
 	List<Livro> livros = Arrays.asList(new Livro(1, "O planeta dos macacos", "Pierre Boulle"),
 			new Livro(2, "172 horas na lua", "Não lembro"));
