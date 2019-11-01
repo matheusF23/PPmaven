6d446adf5d971579963fcb02800418d7734d4971
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2019-11-01 19:20:09 -0300

    Atualização da classe FrameTable do pacote br.ufma.ecp.estante_livros

--- src/main/java/br/ufma/ecp/estante_livros/FrameTable.java
+++ src/main/java/br/ufma/ecp/estante_livros/FrameTable.java
@@ -4,18 +4,22 @@ import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 
+import br.ufma.ecp.estante_livros_prog_reflexiva.GenericTableModel;
+import br.ufma.ecp.estante_livros_prog_reflexiva.ReflectLivroDAO;
+
 public class FrameTable extends JFrame {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	
-	public FrameTable(LivroDAO dao) {
+	public FrameTable() {
 		super("Livros");
-		LivroTableModel tm = new LivroTableModel(dao);
-		JTable t = new JTable(tm);
+		//LivroTableModel tm = new LivroTableModel(dao);
+		//JTable t = new JTable(tm);
+		JTable tm = new JTable(new GenericTableModel<Livro>(new ReflectLivroDAO()));
 		JScrollPane scroll = new JScrollPane();
-		scroll.setViewportView(t);
+		scroll.setViewportView(tm);
 		add(scroll);
 		setSize(800, 250);
 		setLocationRelativeTo(null); // Centraliza o Jframe
