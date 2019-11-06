8afc0a4f79aa9261cfc9ee248277eb308ff6536c
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2019-11-06 19:44:04 -0300

    Criacao da classe Pessoa.

---
+++ src/main/java/br/ufma/ecp/serializacao/Pessoa.java
@@ -0,0 +1,52 @@
+package br.ufma.ecp.serializacao;
+
+import java.io.Serializable;
+
+public class Pessoa implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private String nome;
+    private String cpf;
+    private int idade;
+    private double altura;
+	
+    public Pessoa(String nome, String cpf, int idade, double altura) {
+		super();
+		this.nome = nome;
+		this.cpf = cpf;
+		this.idade = idade;
+		this.altura = altura;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public double getAltura() {
+		return altura;
+	}
+
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+}
